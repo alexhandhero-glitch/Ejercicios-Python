c2df85c93103b52c07c6067ada54b219c07d6045
alexhandhero-glitch <alexhandhero@gmail.com>
2026-05-17 09:34:44 -0500

    contador de vocales

---
+++ .py
@@ -0,0 +1,14 @@
+def contar_vocales(texto):
+    
+    texto = texto.lower()
+    vocales = "aeiouáéíóú"  
+    contador = 0
+    
+    for letra in texto:
+        if letra in vocales:
+            contador += 1  
+            
+    return contador
+
+print(contar_vocales("Python es genial"))   
+print(contar_vocales("MÚSICA"))
