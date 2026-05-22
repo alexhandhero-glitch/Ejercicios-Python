97bed3aa0596e98615db8fa929442bc37883c142
alexhandhero-glitch <alexhandhero@gmail.com>
2026-05-21 22:48:26 -0500

    primer ejercicio

--- tarea_semana3.py
+++ tarea_semana3.py
@@ -1,1 +1,9 @@
 
+# Ejercicio 1. ¿Es el protocolo seguro?
+protocolo = "HTTPS"
+if protocolo == "HTTPS" or protocolo == "SSH" or protocolo == "SFTP":
+    print(f"El protocolo {protocolo} es SEGURO")
+elif protocolo == "HTTP" or protocolo == "Telnet" or protocolo == "FTP":
+    print(f"El protocolo {protocolo} es INSEGURO")
+else:
+    print("Desconocido")
